51e6d7f33d82ce81ad15d5ff966522a748235e1a
Nicolas Orozco <nicolas.orozco@utp.edu.co>
2019-11-26 17:50:25 -0500

    camera.py setup.py

---
+++ Model/camara.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+import time
+
+Builder.load_string('''
+<TakePicture>:
+    orientation: 'vertical'
+    Camera:
+        id: camera
+        resolution: (640, 480)
+        play: False
+    ToggleButton:
+        text: 'capture'
+        on_press: camera.play = not camera.play
+        size_hint_y: None
+        height: '48dp'
+    Button:
+        text: 'Capture'
+        size_hint_y: None
+        height: '48dp'
+        on_press: root.capture()
+''')
+
+
+class TakePicture(BoxLayout):
+    def capture(self):
+        '''
+        Function to capture the images and give them the names
+        according to their captured time and date.
+        '''
+        camera = self.ids['camera']
+        camera.export_to_png("./photo/t.png")
+        print("Captured")
+
+
+class MiCamara(App):
+    def build(self):
+        return TakePicture()
---
+++ View/Setup.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+import kivy
+from kivy.app import App
