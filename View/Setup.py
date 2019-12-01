59a9ec79faf08d904d14dc0214e443153c5379fa
Nicolas Orozco <nicolas.orozco@utp.edu.co>
2019-12-01 10:45:29 -0500

    Setup.y new reg code

--- View/Setup.py
+++ View/Setup.py
@@ -5,6 +5,58 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 
+kv='''
+<MyWidget>
+    spacing: 10
+    padding: 10
+    rows:6
+    id: registro
+
+    BoxLayout:
+        id: cedula
+        Label:
+            text:'Cédula'
+            font_size: 16
+        TextInput:
+            id: key
+            multiline:False
+    BoxLayout:
+        id: nombre
+        Label:
+            text:'Nombre y Apellido'
+            font_size: 16
+        TextInput:
+            multiline:False
+    BoxLayout:
+        id: Correo
+        Label:
+            text:'Correo Electrónico'
+            font_size: 16
+        TextInput:
+            id: mail
+            multiline:False
+    BoxLayout:
+        id: carga
+        Button:
+            text:'Registrar'
+            font_size: 14
+            on_press: root.reg()
+        Button:
+            text:'Cancelar'
+            font_size: 14
+'''
+
+class MyWidget(GridLayout):
+    def __init__(self):
+        super(MyWidget, self).__init__()
+
+    def reg(self):
+        print ('El botón si llama la función')
+
+class registroApp(App):
+    def build(self):
+        Builder.load_string(kv)
+        return MyWidget()
 
 if __name__ == '__main__':
-    pass
+    registroApp().run()
